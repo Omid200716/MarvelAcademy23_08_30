57608de50b0d1a57d8284db9986f9dd0d35d752e
Omid <Omid_nikzad@hotmail.se>
2023-08-31 22:03:40 +0200

    back function

--- app/src/main/java/com/example/marvelacademy23_08_30/SecondActivity.java
+++ app/src/main/java/com/example/marvelacademy23_08_30/SecondActivity.java
@@ -3,12 +3,21 @@ package com.example.marvelacademy23_08_30;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.media.MediaPlayer;
 
 public class SecondActivity extends AppCompatActivity {
 
     TextView tv2;
     Bundle extra;
+
+    ImageView iv;
+
+    ImageView youtube;
+
+   // MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +25,41 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
 
         extra = getIntent().getExtras();
+        iv = findViewById(R.id.back);
+
+        youtube = findViewById(R.id.youtube);
+       // mediaPlayer = MediaPlayer.create(this, R.raw.sound);
+
 
         tv2 = findViewById(R.id.tv2);
         tv2.setText(extra.getString("text"));
+        iv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        youtube.setImageResource(R.drawable.youtube);
+
+        youtube.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+               /* if (mediaPlayer != null) {
+                    if (mediaPlayer.isPlaying()) {
+                        mediaPlayer.pause();
+                    } else {
+                        mediaPlayer.start();
+                    }
+                }
+                */
+
+
+
+
+            }
+        });
 
 
     }
